86a772f64444749c0be9f8c22b1b2fbec5d2ddae
Willem <willem.duminy@gmail.com>
2014-02-22 08:56:53 +0200

    images from r to latex

--- docs/learning_framework/scripts/c4-300.r
+++ docs/learning_framework/scripts/c4-300.r
@@ -4,14 +4,19 @@ outFile <- concat(outDir,"c4-300")
 
 r <- read.table(inFile,header=TRUE,sep=" ")
 attach(r);
+
 # http://stackoverflow.com/questions/1890215/getting-r-plots-into-latex
-pdf(concat(outFile,"cc.pdf"));
+postscript(file=concat(outFile,"cc.eps"), width = 6.0, height = 5.0,
+           horizontal = FALSE,  paper = "special",
+           family = "ComputerModern", encoding = "TeXtext.enc");
 plot(cutoff,certainty,xaxp=c(0,288,9),xaxs="i",type="p");
 sr = split(r,r$fraction);
 for (e in sr) lines(e$cutoff,e$certainty,lty=e[[1]][1]-2);
 dev.off();
 
-pdf(concat(outFile,"cs.pdf"));
+postscript(file=concat(outFile,"cs.eps"),width = 6.0, height = 5.0,
+           horizontal = FALSE,  paper = "special",
+           family = "ComputerModern", encoding = "TeXtext.enc");
 plot(cutoff,size,xaxp=c(0,288,9),xaxs="i",type="p");
 sr = split(r,r$fraction);
 for (e in sr) lines(e$cutoff,e$size,lty=e[[1]][1]-2);
